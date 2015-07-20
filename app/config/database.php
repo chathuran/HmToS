6307d0ec8dea262e92db7e329e6f22bfda3c21a8
Chathuran <dcedannoruwa@gmail.com>
2015-07-20 08:21:07 +0530

    adding files

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'h2c',
-			'username'  => 'root',
-			'password'  => '1234',
+			'host'      => '$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT',
+			'database'  => 'hmtos',
+			'username'  => 'admin1ulBYqT',
+			'password'  => 'HVwJDFrZPYtN',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
